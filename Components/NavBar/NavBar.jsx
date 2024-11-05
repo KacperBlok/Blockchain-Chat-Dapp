a9d58075bf8ae427d432e28c360613665e6a9924
Kacper Blok <kblok1000@gmail.com>
2024-11-05 16:24:02 +0100

    navbar-update

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -3,13 +3,21 @@ import Image from 'next/image'
 import Link from 'next/link'
 //Creating UI/UX
 
-import styles from './NavBar.module.css'; 
+import Style from './NavBar.module.css'; 
 import { ChatAppContext } from '../../Context/ChatAppContext';
 import { Model, Error } from '../index';
 
 const NavBar = () => {
+    const menuItems = [
+      {
+        menu: "All Users",
+        link: "alluser",
+      }
+    ]
   return (
-    <div>NavBar</div>
+    <div className={Style.Navbar}>
+      <div className={Style.NavBar_box}></div>
+    </div>
   )
 }
 
